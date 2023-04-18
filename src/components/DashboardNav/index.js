1c89a9482f594a910942d272c7ca416fe5bba037
Fatenkh7 <khodorfaten5@gmail.com>
2023-04-18 14:55:50 +0300

    design

--- src/components/DashboardNav/index.js
+++ src/components/DashboardNav/index.js
@@ -1,3 +1,38 @@
-export default function DashboardNav(){
-    return(<>Nav</>)
+import { Layout, Menu } from "antd";
+const { Header } = Layout;
+
+export default function DashboardNav() {
+  return (
+    <Layout>
+      <Header
+        style={{
+          position: "sticky",
+          top: 0,
+          zIndex: 1,
+          width: "100%",
+          height: "32vh",
+          borderRadius: "0.2vh",
+        }}
+      >
+        <div
+          style={{
+            float: "left",
+            width: 120,
+            height: 31,
+            margin: "16px 24px 16px 0",
+            background: "rgba(255, 255, 255, 0.2)",
+          }}
+        />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["2"]}
+          items={new Array(3).fill(null).map((_, index) => ({
+            key: String(index + 1),
+            label: `nav ${index + 1}`,
+          }))}
+        />
+      </Header>
+    </Layout>
+  );
 }
