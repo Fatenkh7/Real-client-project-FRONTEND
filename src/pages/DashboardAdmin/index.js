2b7791d2f20b19480b024615889f10815668025f
Fatenkh7 <khodorfaten5@gmail.com>
2023-04-21 01:04:53 +0300

    edit the switch

--- src/pages/DashboardAdmin/index.js
+++ src/pages/DashboardAdmin/index.js
@@ -1,15 +1,18 @@
-import { Table, Space, Input } from "antd";
-import { EditOutlined, DeleteOutlined, CloseOutlined } from "@ant-design/icons";
+import { Table, Space, Input, Switch, Form } from "antd";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./index.css";
 import Button from "../../components/Button";
 import Popup from "../../components/Popup";
+import Swal from "sweetalert2";
 
 export default function Home() {
   const [data, setData] = useState([]);
   const [addPop, setAddPop] = useState(false);
   const [editPop, setEditPop] = useState(false);
+  const [selectedRecord, setSelectedRecord] = useState(null);
+
   const closePop = () => {
     setAddPop(false);
     setEditPop(false);
@@ -28,13 +31,32 @@ export default function Home() {
   }, []);
 
   const handleEdit = (record) => {
-    console.log(`Edit record ${record.key}`);
-    // handle edit here
+    setSelectedRecord(record);
+    setEditPop(true);
   };
 
   const handleDelete = (record) => {
-    console.log(`Delete record ${record.key}`);
-    // handle delete here
+    setSelectedRecord(record);
+    Swal.fire({
+      title: "Are you sure you want to delete this Admin?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3a70a1",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axios
+          .delete(`http://localhost:5000/admin/${record.key}`)
+          .then((response) => {
+            setData(response.data.response);
+            console.log(response.data.response);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    });
   };
 
   const columns = [
@@ -50,7 +72,7 @@ export default function Home() {
       title: "First Name",
       dataIndex: "firstName",
       defaultSortOrder: "descend",
-      sorter: (a, b) => a.firstName - b.firstName,
+      sorter: (a, b) => a.firstName.localeCompare(b.firstName),
     },
     {
       title: "Last Name",
@@ -73,8 +95,19 @@ export default function Home() {
     {
       title: "Is Super",
       dataIndex: "isSuper",
-      filters: [],
+      filters: [
+        { text: "Yes", value: "yes" },
+        { text: "No", value: "no" },
+      ],
       onFilter: (value, record) => record.isSuper.indexOf(value) === 0,
+      render: (isSuper, record) => (
+        <Switch
+          checked={isSuper}
+          onChange={() => {
+            record.isSuper = !record.isSuper;
+          }}
+        />
+      ),
     },
     {
       title: "Actions",
@@ -84,7 +117,27 @@ export default function Home() {
           <a onClick={() => handleEdit(record)}>
             <EditOutlined />
           </a>
-          <a onClick={() => handleDelete(record)}>
+          <a
+            onClick={() =>
+              Swal.fire({
+                title: "Are you sure you want to delete this Admin?",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3a70a1",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, delete it!",
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  handleDelete(record);
+                  Swal.fire(
+                    "Deleted!",
+                    "Your admin has been deleted.",
+                    "success"
+                  );
+                }
+              })
+            }
+          >
             <DeleteOutlined />
           </a>
         </Space>
@@ -100,8 +153,18 @@ export default function Home() {
     setAddPop(true);
     console.log("clicked");
   };
-  const handleSubmit = () => {
-    console.log("Add new admin");
+  const handleSubmit = (record) => {
+    axios
+      .post("http://localhost:5000/admin/add", record)
+      .then((response) => {
+        setData([...data, response]); // add the new admin to the existing data array
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+    setSelectedRecord(null); // clear the selected record
+    setEditPop(false); // hide the popup
   };
 
   return (
@@ -119,7 +182,7 @@ export default function Home() {
       />
       {addPop && (
         <Popup title="Add Admin" close={closePop}>
-          <div>
+          <div className="input-container">
             <Input
               id="outlined-controlled"
               placeholder="First Name"
@@ -144,33 +207,151 @@ export default function Home() {
               name="email"
               allowClear
             />
-            <Input
-              id="outlined-uncontrolled"
-              placeholder="Is Super"
-              name="is_super"
-              allowClear
-            />
+            <Form.Item label="Is Super">
+              <Switch
+                id="outlined-uncontrolled"
+                placeholder="Is Super"
+                name="is_super"
+                allowClear
+              />
+            </Form.Item>
             <Input
               id="outlined-uncontrolled"
               placeholder="Password"
               name="password"
               allowClear
             />
-            <Button
-              variant="contained"
-              disableElevation
-              style={{ height: 55 }}
-              sx={{ backgroundColor: "#3d0066" }}
+            <button
+              type="primary"
+              ghost
+              shape="round"
+              size="middle"
+              style={{
+                backgroundColor: "#37a2f5",
+                border: "none",
+                color: "white",
+                fontSize: "14px",
+                padding: "12px 12px",
+                borderRadius: "4px",
+                display: "block",
+                margin: "0 auto",
+                marginTop: "10px",
+              }}
               onClick={() => {
-                handleSubmit();
-                setAddPop(false);
+                Swal.fire({
+                  title: "Are you sure you want to added?",
+                  icon: "question",
+                  showCancelButton: true,
+                  confirmButtonColor: "#3a70a1",
+                  cancelButtonColor: "#d33",
+                  confirmButtonText: "Yes, add it!",
+                }).then((result) => {
+                  if (result.isConfirmed) {
+                    handleSubmit();
+                    setEditPop(false);
+                    Swal.fire(
+                      "Added!",
+                      "Your Admin has been added.",
+                      "success"
+                    );
+                  }
+                });
               }}
             >
               Submit
-            </Button>
+            </button>
           </div>
         </Popup>
       )}
+      {editPop &&
+        selectedRecord && ( // show popup if editPop is true and selectedRecord is not null
+          <Popup title="Edit Admin" close={closePop}>
+            <div className="input-container">
+              <Input
+                id="outlined-controlled"
+                placeholder="First Name"
+                name="first_name"
+                allowClear
+                value={selectedRecord.firstName} // populate input fields with selected record data
+              />
+              <Input
+                id="outlined-uncontrolled"
+                placeholder="Last Name"
+                name="last_name"
+                allowClear
+                value={selectedRecord.lastName}
+              />
+              <Input
+                id="outlined-uncontrolled"
+                placeholder="Username"
+                name="username"
+                allowClear
+                value={selectedRecord.userName}
+              />
+              <Input
+                id="outlined-uncontrolled"
+                placeholder="Email"
+                name="email"
+                allowClear
+                value={selectedRecord.email}
+              />
+            <Form.Item label="Is Super">
+              <Switch
+                id="outlined-uncontrolled"
+                placeholder="Is Super"
+                name="is_super"
+                allowClear
+                value={selectedRecord.isSuper}
+              />
+            </Form.Item>
+              <Input
+                id="outlined-uncontrolled"
+                placeholder="Password"
+                name="password"
+                allowClear
+              />
+              <button
+                type="primary"
+                ghost
+                shape="round"
+                size="middle"
+                style={{
+                  backgroundColor: "#37a2f5",
+                  border: "none",
+                  color: "white",
+                  fontSize: "14px",
+                  padding: "12px 12px",
+                  borderRadius: "4px",
+                  display: "block",
+                  margin: "0 auto",
+                  marginTop: "10px",
+                }}
+                onClick={() => {
+                  Swal.fire({
+                    title: "Are you sure you want to edit this admin?",
+                    icon: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: "#3a70a1",
+                    cancelButtonColor: "#d33",
+                    confirmButtonText: "Yes, edit it!",
+                  }).then((result) => {
+                    if (result.isConfirmed) {
+                      handleSubmit();
+                      setEditPop(false);
+                      Swal.fire(
+                        "Edited!",
+                        "Your admin has been edited.",
+                        "success"
+                      );
+                    }
+                  });
+                }}
+              >
+                Edit
+              </button>
+            </div>
+          </Popup>
+        )}
     </div>
   );
 }
