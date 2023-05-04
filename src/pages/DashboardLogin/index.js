0f7d3829783686b8c7e48e04677df2510892fa8d
Fatenkh7 <khodorfaten5@gmail.com>
2023-05-04 10:10:27 +0300

    swap

--- src/pages/DashboardLogin/index.js
+++ src/pages/DashboardLogin/index.js
@@ -27,9 +27,10 @@ export default function Home() {
       });
       const cookies = new Cookies();
       cookies.set("token", response.data.token);
+      btnRef.current.classList.add("bubble-swap");
       // Redirect to dashboard page after successful login
       window.location = "/dashboard";
-      alert(" Login Succssed ");
+      //   alert(" Login Succssed ");
     } catch (error) {
       console.error(error);
       // Show error message to user
@@ -110,6 +111,18 @@ export default function Home() {
             </div>
           </form>
         </div>
+      </div>
+      <div id="background-wrap">
+        <div class="bubble x1"></div>
+        <div class="bubble x2"></div>
+        <div class="bubble x3"></div>
+        <div class="bubble x4"></div>
+        <div class="bubble x5"></div>
+        <div class="bubble x6"></div>
+        <div class="bubble x7"></div>
+        <div class="bubble x8"></div>
+        <div class="bubble x9"></div>
+        <div class="bubble x10"></div>
       </div>
     </div>
   );
