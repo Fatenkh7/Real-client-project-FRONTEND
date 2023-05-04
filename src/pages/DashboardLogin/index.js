44454a28f833d928263201e815f428319e2d1b20
Fatenkh7 <khodorfaten5@gmail.com>
2023-05-04 16:54:05 +0300

    login done

--- src/pages/DashboardLogin/index.js
+++ src/pages/DashboardLogin/index.js
@@ -47,8 +47,8 @@ export default function Home() {
           <canvas id="canvas" className="canvas-back"></canvas>
           <motion.div
             className="backLeft"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
+            initial={{ opacity: 0, transition:{duration:1} }}
+            animate={{ opacity: 1, transition:{duration:1} }}
             exit={{ opacity: 0 }}
           ></motion.div>
         </div>
