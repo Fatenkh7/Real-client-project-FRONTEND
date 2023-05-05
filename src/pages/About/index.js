f8e49ecbe3fa3237341b915bb8ce5b63cd83701f
Nabigha Mogharbel <nabighamogharbel@gmail.com>
2023-05-05 18:11:42 +0300

    animated routes trial

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,3 +1,9 @@
+import {motion} from "framer-motion"
 export default function Home(){
-    return(<>Home</>)
+    return(<motion.div
+        initial={{ opacity: 0, x:-10 }}
+      animate={{ opacity: 1, x:0 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 1.5 }}
+    >Home</motion.div>)
 }
